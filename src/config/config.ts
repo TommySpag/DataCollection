388819732f2894f716191d146f11a01fd2757cea
TommySpag <tommyspagnuolo@gmail.com>
2024-11-08 15:05:39 -0500

    Config de HTTPS en fonction de l'env

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,11 +5,12 @@ dotenv.config();
 
 export const config = {
   port: process.env.PORT || 3000,
-  CERT_KEY: process.env.CERT_KEY || './certificates/key.pem',
-  CERT_CERT: process.env.CERT_CERT || './certificates/cert.pem',
-  sessionSecret: process.env.SESSION_SECRET || 'secret_par_defaut_pour_les_sessions',
-  jwtSecret: process.env.JWT_SECRET || 'secret_par_defaut_pour_le_jwt',
-  databaseUrl: process.env.DATABASE_URL || 'mongodb://localhost:27017/defaultdb',
-  nodeEnv: process.env.NODE_ENV || 'development',
-  isProduction: process.env.NODE_ENV === 'production',
+  CERT_KEY: process.env.CERT_KEY || '',
+  CERT_CERT: process.env.CERT_CERT || '',
+  sessionSecret: process.env.SESSION_SECRET || '',
+  jwtSecret: process.env.JWT_SECRET || '',
+  databaseUrl: process.env.DATABASE_URL || '',
+  nodeEnv: process.env.NODE_ENV || '',
+  ip: process.env.IP_A || '',
+  isProduction: process.env.NODE_ENV === 'production'
 };
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,20 @@ import {logger} from './utils/logger';
 import https from 'https';
 import fs from 'fs';
 import path from 'path';
+import os from 'os';
 
+function getLocalIPAddress() {
+  const networkInterfaces = os.networkInterfaces();
+  for (const interfaceName in networkInterfaces) {
+    const addresses = networkInterfaces[interfaceName];
+    for (const address of addresses ?? []) {
+      if (address.family === 'IPv4' && !address.internal) {
+        return address.address;
+      }
+    }
+  }
+  return 'IP address not found';
+}
 
 // Middleware de session avec la clé secrète provenant des variables de configuration
 app.use(session({
@@ -17,16 +30,17 @@ app.use(session({
   }
 }));
 
-//JUST IN DEV
 const httpsOptions: https.ServerOptions = {  key: fs.readFileSync(path.resolve(config.CERT_KEY ?? "")),  cert: fs.readFileSync(path.resolve(config.CERT_CERT ?? "")),};
 const port = config.port;
+const ip = getLocalIPAddress();
 
 // Démarrage du serveur
-
-function start() {
-  logger.info('Demarrage app');
-  https.createServer(httpsOptions, app).listen(port, () => {  console.log(`Server is running on https://localhost:${port}`);});
-  fetchProducts();
+if(config.nodeEnv == 'production') {
+    logger.info('Demarrage app');
+    app.listen(port, () => {  console.log(`Server is running on https://${ip}:${port}`);});
+    fetchProducts();
+} else {
+    logger.info('Demarrage app');
+    https.createServer(httpsOptions, app).listen(port, () => {  console.log(`Server is running on https://${ip}:${port}`);});
+    fetchProducts();
 }
-
-start();
